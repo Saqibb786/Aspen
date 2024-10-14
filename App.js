503c2f260a7c7672785d0e9026024b3495876e7e
Abdullah Naveed <abdullahnaveeed01@gmail.com>
2024-10-14 20:39:30 +0500

    Update home.js

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Home from "./src/screens/Home";
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <Home/>
   );
 };
 
