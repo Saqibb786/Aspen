372c39e533283f859a282acf2e4d0573866745c2
Abdullah Naveed <abdullahnaveeed01@gmail.com>
2024-10-14 21:25:39 +0500

    add background image in splash screen

--- src/screens/splash/Splash.js
+++ src/screens/splash/Splash.js
@@ -1,13 +1,16 @@
-import { Text, View } from "react-native";
+import { Text, View, ImageBackground } from "react-native";
 import React from "react";
 import styles from "./styles";
 
 const Splash = () => {
   return (
     <View style={styles.main}>
-      <View style={styles.container}>
-        <Text style={styles.text}>Splash Screen</Text>
-      </View>
+      <ImageBackground
+        source={require("../../assets/Splash_background.png")}
+        style={styles.background}
+      >
+        <Text style={styles.text}>Welcome to the App</Text>
+      </ImageBackground>
     </View>
   );
 };
--- src/screens/splash/styles.js
+++ src/screens/splash/styles.js
@@ -1,21 +1,21 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
-    main: {
-        flex: 1,
-        width: "100%",
-        height: "100%",
-      },
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: "center",
-    },
-    text: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#000',
-    },
-
+  main: {
+    flex: 1,
+    
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#000",
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
 });
 
 export default styles;
