b4eab5f996a73e50acb715a9fca70fd4cebdaa41
Abdullah Naveed <abdullahnaveeed01@gmail.com>
2024-10-22 21:04:56 +0500

    update package.json

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import RootStackNavigation from "./src/navigation/RootStackNavigation";
-import ParticularScreen from "./src/screens/particularScreen/ParticularScreen";
+
 
 const App = () => {
   return (
     <RootStackNavigation />
-    // <ParticularScreen />
   );
 };
 
